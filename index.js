a4dfae9c61f80ae2520841a1fa0f7fda4efaffa4
trentendresen <37137772+trentendresen@users.noreply.github.com>
2020-12-17 22:59:16 -0600

    Base skeleton for the project essentially all the pages and the navbar

---
+++ index.js
@@ -0,0 +1,31 @@
+let express = require('express');
+let app = express();
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+
+app.get('/',function(req,res){
+    res.render('home');
+});
+
+app.get('/aboutme',function(req,res){
+    res.render('aboutme')
+});
+
+app.get('/contact',function(req,res){
+    res.render('contact')
+});
+
+app.get('/portfolio',function(req,res){
+    res.render('portfolio')
+});
+
+app.listen(port,() => {
+    console.log("App listening on port: "+port);
+})
